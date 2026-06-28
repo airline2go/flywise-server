0715dd81b1969e271b5c8098e70843bc6b7839c0
airline2go <ghytly25@gmail.com>
2026-06-28 14:14:55 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1117,7 +1117,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
